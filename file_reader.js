f7e541a99e16be3d65581581bc189f079f117223
Jorge Ortega <jorgeloscr@hotmail.com>
2024-07-24 17:29:50 -0700

    completed

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  console.log(data1)
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    console.log(data2)
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      console.log(data3)
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        console.log(data4)
+        console.log(`${data1} ${data2} is ${50} years old and his hoobies are ${data4}`)
+      })
+    })
+  })
+   
 })
